f8a2ce6a43a45e2398cd34434aa2fa4f884cbd78
Kimili2003 <lijinfeng2003@163.com>
2024-07-18 13:29:12 -0400

    Change config.js

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:NjI2OS1saWppbmZl@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:ODQzOC1saWppbmZl@127.0.0.1:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
