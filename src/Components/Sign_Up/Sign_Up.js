ab12e5170f0d4d71e3f6e7569fe1cbc4603bf68b
Kimili2003 <lijinfeng2003@163.com>
2024-07-17 20:59:45 -0400

    change sign in and nav bar page

--- src/Components/Sign_Up/Sign_Up.js
+++ src/Components/Sign_Up/Sign_Up.js
@@ -1,123 +1,98 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import "./Sign_Up.css";
+import React, { useState } from 'react';
+import './Sign_Up.css';
+import { Link, useNavigate } from 'react-router-dom';
+import { API_URL } from '../../config';
 
+// Function component for Sign Up form
 const Sign_Up = () => {
-    const [formData, setFormData] = useState({
-        name: '',
-        phone: '',
-        email: '',
-        password: ''
-    });
+    // State variables using useState hook
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [password, setPassword] = useState('');
+    const [showerr, setShowerr] = useState(''); // State to show error messages
+    const navigate = useNavigate(); // Navigation hook from react-router
 
-    const [errors, setErrors] = useState({});
+    // Function to handle form submission
+    const register = async (e) => {
+        e.preventDefault(); // Prevent default form submission
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({
-            ...formData,
-            [name]: value
+        // API Call to register user
+        const response = await fetch(`${API_URL}/api/auth/register`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                name: name,
+                email: email,
+                password: password,
+                phone: phone,
+                role: 'user' // 添加角色信息到请求负载
+            }),
         });
-    };
 
-    const validate = () => {
-        let inputErrors = {};
+        const json = await response.json(); // Parse the response JSON
 
-        if (!formData.name) {
-            inputErrors.name = "Name is required";
-        }
+        if (json.authtoken) {
+            // Store user data in session storage
+            sessionStorage.setItem("auth-token", json.authtoken);
+            sessionStorage.setItem("name", name);
+            sessionStorage.setItem("phone", phone);
+            sessionStorage.setItem("email", email);
 
-        if (!formData.phone) {
-            inputErrors.phone = "Phone number is required";
-        } else if (formData.phone.length !== 10 || !/^\d{10}$/.test(formData.phone)) {
-            inputErrors.phone = "Phone number should be 10 digits";
-        }
-
-        if (!formData.email) {
-            inputErrors.email = "Email is required";
-        } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
-            inputErrors.email = "Email address is invalid";
-        }
-
-        if (!formData.password) {
-            inputErrors.password = "Password is required";
-        }
-
-        setErrors(inputErrors);
-        return Object.keys(inputErrors).length === 0;
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (validate()) {
-            console.log('Form submitted', formData);
-            alert('Form submitted successfully');
-            window.location.reload()
+            // Redirect user to home page
+            navigate("/");
+            window.location.reload(); // Refresh the page
         } else {
-            console.log('Validation failed');
+            if (json.errors) {
+                for (const error of json.errors) {
+                    setShowerr(error.msg); // Show error messages
+                }
+            } else {
+                setShowerr(json.error);
+            }
         }
     };
 
+    // JSX to render the Sign Up form
     return (
         <div className="container" style={{ marginTop: '5%' }}>
             <div className="signup-grid">
-                <div className="signup-text">
-                    <h1>Sign Up</h1>
-                </div>
-                <div className="signup-text1" style={{ textAlign: 'left' }}>
-                    Already a member? <span><Link to="/login" style={{ color: '#2190FF' }}>Login</Link></span>
-                </div>
                 <div className="signup-form">
-                    <form onSubmit={handleSubmit}>
+                    <form method="POST" onSubmit={register}>
                         <div className="form-group">
                             <label htmlFor="name">Name</label>
-                            <input type="text" name="name" id="name" required className="form-control"
-                                   placeholder="Enter your name"
-                                   value={formData.name}
-                                   onChange={handleChange}
-                                   aria-describedby="helpId"/>
-                            {errors.name && <div className="error" style={{ color: 'red' }}>{errors.name}</div>}
+                            <input value={name} onChange={(e) => setName(e.target.value)} type="text" name="name" id="name" className="form-control" placeholder="Enter your name" aria-describedby="helpId" />
                         </div>
-
                         <div className="form-group">
                             <label htmlFor="phone">Phone</label>
-                            <input type="tel" name="phone" id="phone" required className="form-control"
-                                   placeholder="Enter your phone number"
-                                   value={formData.phone}
-                                   onChange={handleChange}
-                                   aria-describedby="helpId"/>
-                            {errors.phone && <div className="error" style={{ color: 'red' }}>{errors.phone}</div>}
+                            <input value={phone} onChange={(e) => setPhone(e.target.value)} type="tel" name="phone" id="phone" className="form-control" placeholder="Enter your phone number" aria-describedby="helpId" />
                         </div>
-
                         <div className="form-group">
                             <label htmlFor="email">Email</label>
-                            <input type="email" name="email" id="email" required className="form-control"
-                                   placeholder="Enter your email"
-                                   value={formData.email}
-                                   onChange={handleChange}
-                                   aria-describedby="helpId"/>
-                            {errors.email && <div className="error" style={{ color: 'red' }}>{errors.email}</div>}
+                            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" name="email" id="email" className="form-control" placeholder="Enter your email" aria-describedby="helpId" />
                         </div>
-
                         <div className="form-group">
                             <label htmlFor="password">Password</label>
-                            <input type="password" name="password" id="password" required className="form-control"
-                                   placeholder="Enter your password"
-                                   value={formData.password}
-                                   onChange={handleChange}
-                                   aria-describedby="helpId"/>
-                            {errors.password && <div className="error" style={{ color: 'red' }}>{errors.password}</div>}
+                            <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" name="password" id="password" className="form-control" placeholder="Enter your password" aria-describedby="helpId" />
                         </div>
-
+                        {showerr && <div className="err" style={{ color: 'red' }}>{showerr}</div>}
                         <div className="btn-group">
                             <button type="submit" className="btn btn-primary mb-2 mr-1 waves-effect waves-light">Submit</button>
-                            <button type="reset" className="btn btn-danger mb-2 waves-effect waves-light" onClick={() => setFormData({ name: '', phone: '', email: '', password: '' })}>Reset</button>
+                            <button type="reset" className="btn btn-danger mb-2 waves-effect waves-light" onClick={() => {
+                                setName('');
+                                setPhone('');
+                                setEmail('');
+                                setPassword('');
+                                setShowerr('');
+                            }}>Reset</button>
                         </div>
                     </form>
                 </div>
             </div>
         </div>
     );
-};
+}
 
-export default Sign_Up;
+export default Sign_Up; // Export the Sign_Up component for use in other components
