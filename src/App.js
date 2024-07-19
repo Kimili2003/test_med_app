d861f84c7bc21135ed1adf91df60e208e6a9f842
Kimili2003 <lijinfeng2003@163.com>
2024-07-19 15:58:16 -0400

    Integrated Instant Consultation component

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Navbar from './Components/Navbar/Navbar';
 import Landing_page from './Components/Landing_Page/LandingPage';
 import Sign_Up from "./Components/Sign_Up/Sign_Up";
 import Login from "./Components/Login/Login";
+import InstantConsultation from "./Components/InstantConsultationBooking/InstantConsultation";
 
 // Function component for the main App
 function App() {
@@ -31,6 +32,10 @@ function App() {
 
                         {/* Route for the Login page */}
                         <Route path="/login" element={<Login />} />
+
+                        {/*Route for the InstantConsultation*/}
+                        <Route path="/instant-consultation" element={<InstantConsultation />} />
+
                 </Routes>
             </BrowserRouter>
         </div>
